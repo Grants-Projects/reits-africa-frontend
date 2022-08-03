891b335780c9ffd7b7635d7aaff336a5ea0142d4
unknown <ademayowaadeniyi@gmail.com>
2022-08-03 23:13:31 +0100

    wallet page

--- data.js
+++ data.js
@@ -18,3 +18,30 @@ export const data = [
     balance: 'My Balance',
   },
 ];
+
+export const depositData = [
+  {
+    id: 1,
+    text: 'Total Asset',
+    amount: '0.00 NGN',
+    balance: 'Balance',
+  },
+  {
+    id: 2,
+    text: 'Crypto Balance',
+    amount: '0.00 NEAR',
+    balance: 'Balance',
+  },
+  {
+    id: 3,
+    text: 'Trading Account',
+    amount: '0.00 NGN',
+    balance: 'Balance',
+  },
+  {
+    id: 4,
+    text: 'Trading Account',
+    amount: '0.00 NGN',
+    balance: 'Balance',
+  },
+];
