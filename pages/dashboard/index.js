86d8c4b190dcdc50da2cb0043654a5b7466dd133
unknown <ademayowaadeniyi@gmail.com>
2022-08-03 19:12:02 +0100

    refactored feed pages

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -6,7 +6,7 @@ import Transactions from 'pages/transactions';
 export default function DashboardPage() {
   return (
     <Layout title='Dashboard'>
-      <div className='md:9/12 md:ml-40 lg:ml-60 md:px-10 mt-10 max-w-[2560px] mb-20'>
+      <div className='md:9/12 md:ml-28 lg:ml-52 md:px-10 mt-10 max-w-[2560px] mb-20'>
         <div className='flex flex-col xxl:ml-60'>
           <Sidebar />
           <Dashboard />
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -5,7 +5,7 @@ import Layout from '@/components/Layout';
 export default function PortfolioPage() {
   return (
     <Layout title='Portfolio'>
-      <div className='md:9/12 md:ml-40 lg:ml-60 md:px-10 mt-10 max-w-[2560px] mb-20'>
+      <div className='md:9/12 md:ml-28 lg:ml-52 md:px-10 mt-10 max-w-[2560px] mb-20'>
         <div className='flex flex-col xxl:ml-60'>
           <Sidebar />
           <Portfolio />
