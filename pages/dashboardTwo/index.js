30777564ba9ad90a53a6cab96363848de87f45c1
unknown <ademayowaadeniyi@gmail.com>
2022-08-03 17:24:26 +0100

    added dashboardTwo screen

---
+++ pages/dashboardTwo/index.js
@@ -0,0 +1,51 @@
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
+import { data } from '../../data';
+
+export default function DashboardTwo() {
+  return (
+    <div className='mb-10'>
+      <div>
+        <h1 className='text-base mt-5'>Welcome</h1>
+        <h2 className='text-[28px] md:text-[48px]'>Hello, Marv</h2>
+      </div>
+
+      <div className='wrapper'>
+        <div className='flex items-center justify-between mt-3'>
+          <h3 className='text-lg md:text-2xl'>Overview</h3>
+          <div className='flex space-x-3'>
+            <button className='bg-[#EC3335] sm:text-md text-white shadow-2xl px-6 py-2 rounded-md ml-auto'>
+              Buy Tokens
+            </button>
+            <button className='bg-[#EDF2F7] sm:text-md text-black font-semibold shadow-2xl px-6 py-2 rounded-md'>
+              Trade
+            </button>
+          </div>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-3 gap-4 mt-5'>
+          {data.map((item) => (
+            <div
+              key={item.id}
+              className='flex bg-white shadow-md rounded-md py-8 px-8 md:px-10 lg:px-10'
+            >
+              {/* CardList */}
+              <div>
+                <h4 className='text-[#747475] text-sm font-medium'>
+                  {item.text}
+                </h4>
+                <p className='text-[24px] text-[#27262E] my-3 font-medium'>
+                  {item.amount}
+                </p>
+
+                <div className='flex items-center bg-[#F2F4FA] rounded-lg px-4 py-2'>
+                  <span className='text-sm font-bold'>{item.balance}</span>
+                  <MdOutlineArrowForwardIos className='w-3 h-3 ml-1 text-[#817C8E]' />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
