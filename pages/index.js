f5744552dbcba8f1526b0ce8195aad760cbe0196
unknown <ademayowaadeniyi@gmail.com>
2022-08-04 15:39:26 +0100

    added:wallet withdrawal & wallet withdrawal info

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Layout from '@/components/Layout';
+import WalletWithdrawal from '@/components/WalletWithdrawal/WalletWithdrawal';
 
 export default function Home() {
   return (
     <Layout title='Reits Africa | Home'>
       <main>
         <h2>HomePage</h2>
+        <WalletWithdrawal />
       </main>
     </Layout>
   );
---
+++ pages/withdrawal/index.js
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout';
+
+export default function Withdrawal() {
+  return (
+    <Layout title='Withdrawal'>
+      <h2>Withdrawal Page</h2>
+      {/* stepone */}
+      {/* steptwo */}
+      {/* stepthree */}
+      {/* stepfour */}
+    </Layout>
+  );
+}
