edc39e97079950ed8c4109e727be81226657e5a7
unknown <ademayowaadeniyi@gmail.com>
2022-08-05 22:46:22 +0100

    UI mobile adjustments

--- components/ExchangeOverview/ExchangeOverview.js
+++ components/ExchangeOverview/ExchangeOverview.js
@@ -8,7 +8,7 @@ export default function ExchangeOverview() {
   return (
     <div className='mb-10 px-5'>
       <div className='wrapper'>
-        <div className='flex items-center justify-between mt-3'>
+        <div className='flex space-x-3 items-center justify-between mt-3'>
           <h3 className='text-lg md:text-2xl'>Exchange</h3>
           <div className='flex space-x-3 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-stone-300'>
             <button className='bg-[#EC3335] sm:text-md text-white shadow-sm mb-3 px-6 py-2 rounded-md ml-auto'>
